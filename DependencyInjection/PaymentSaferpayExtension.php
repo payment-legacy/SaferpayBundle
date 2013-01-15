5955172a0d0addb6880ae50bacb40b4d95cea2a0
Dominik Zogg <dominik.zogg@gmail.com>
2013-01-15 21:45:39 +0100

    config done, and started with dic

---
+++ DependencyInjection/PaymentSaferpayExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Payment\Bundle\SaferpayBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class IbrowsSaferpayExtension extends Extension
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $processor = new Processor();
+        $config = $processor->process($configuration->getTree(), $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
+        $container->setParameter('payment.saferpay', $config);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlias()
+    {
+        return 'payment_saferpay';
+    }
+}
