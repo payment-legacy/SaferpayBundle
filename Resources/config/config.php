ccad9ed16c2127c9a69c325dcee0074ae68d61e6
Dominik Zogg <dominik.zogg@gmail.com>
2013-01-16 21:25:30 +0100

    make dic code more flexible

--- Resources/config/config.php
+++ Resources/config/config.php
@@ -2,23 +2,28 @@
 
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Container;
+
+/** @var Container $container */
+
+$container->setParameter('payment.saferpay.key_value.class', 'Payment\Saferpay\SaferpayKeyValue');
+$container->setParameter('payment.saferpay.config.class', 'Payment\Saferpay\SaferpayConfig');
+$container->setParameter('payment.saferpay.class', 'Payment\Saferpay\Saferpay');
 
 $container
-    ->setDefinition('payment_saferpay_config', new Definition('Payment\Saferpay\SaferpayConfig'))
+    ->setDefinition('payment.saferpay.config', new Definition('%payment.saferpay.config.class%'))
     ->addMethodCall('setInitUrl', array('%payment.saferpay.urls.init%'))
     ->addMethodCall('setConfirmUrl', array('%payment.saferpay.urls.confirm%'))
     ->addMethodCall('setCompleteUrl', array('%payment.saferpay.urls.complete%'))
-    ->addMethodCall('setInitValidationsConfig', array(new Definition('Payment\Saferpay\SaferpayKeyValue', array('%payment.saferpay.validators.init%'))))
-    ->addMethodCall('setConfirmValidationsConfig', array(new Definition('Payment\Saferpay\SaferpayKeyValue', array('%payment.saferpay.validators.confirm%'))))
-    ->addMethodCall('setCompleteValidationsConfig', array(new Definition('Payment\Saferpay\SaferpayKeyValue', array('%payment.saferpay.validators.complete%'))))
-    ->addMethodCall('setInitDefaultsConfig', array(new Definition('Payment\Saferpay\SaferpayKeyValue', array('%payment.saferpay.defaults.init%'))))
-    ->addMethodCall('setConfirmDefaultsConfig', array(new Definition('Payment\Saferpay\SaferpayKeyValue', array('%payment.saferpay.defaults.confirm%'))))
-    ->addMethodCall('setCompleteDefaultsConfig', array(new Definition('Payment\Saferpay\SaferpayKeyValue', array('%payment.saferpay.defaults.complete%'))))
+    ->addMethodCall('setInitValidationsConfig', array(new Definition('%payment.saferpay.key_value.class%', array('%payment.saferpay.validators.init%'))))
+    ->addMethodCall('setConfirmValidationsConfig', array(new Definition('%payment.saferpay.key_value.class%', array('%payment.saferpay.validators.confirm%'))))
+    ->addMethodCall('setCompleteValidationsConfig', array(new Definition('%payment.saferpay.key_value.class%', array('%payment.saferpay.validators.complete%'))))
+    ->addMethodCall('setInitDefaultsConfig', array(new Definition('%payment.saferpay.key_value.class%', array('%payment.saferpay.defaults.init%'))))
+    ->addMethodCall('setConfirmDefaultsConfig', array(new Definition('%payment.saferpay.key_value.class%', array('%payment.saferpay.defaults.confirm%'))))
+    ->addMethodCall('setCompleteDefaultsConfig', array(new Definition('%payment.saferpay.key_value.class%', array('%payment.saferpay.defaults.complete%'))))
 ;
 
 $container
-    ->setDefinition('payment_saferpay', new Definition('Payment\Saferpay\Saferpay'))
-    ->addMethodCall('setConfig', array(new Reference('payment_saferpay_config')))
-    ->addMethodCall('setHttpClient', array(new Definition('Payment\Saferpay\Http\Client\GuzzleClient')))
-    //->addMethodCall('setLogger', array(new Reference('logger')))
+    ->setDefinition('payment.saferpay', new Definition('%payment.saferpay.class%'))
+    ->addMethodCall('setConfig', array(new Reference('payment.saferpay.config')))
 ;
