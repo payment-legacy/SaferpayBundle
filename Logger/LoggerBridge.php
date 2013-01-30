c99a5f77d3c9b19a3bde67f30c54bfebb127c735
Mike Meier <mike.meier@ibrows.ch>
2013-01-30 15:15:18 +0100

    add LoggerBridge for symfony 2.1.* implementation of LoggerInterface

---
+++ Logger/LoggerBridge.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Payment\Bundle\SaferpayBundle\Logger;
+
+use Symfony\Bridge\Monolog\Logger;
+use Psr\Log\LoggerInterface;
+
+class LoggerBridge implements LoggerInterface
+{
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function emergency($message, array $context = array())
+    {
+        return $this->logger->emerg($message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function alert($message, array $context = array())
+    {
+        return $this->logger->alert($message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function critical($message, array $context = array())
+    {
+        return $this->logger->crit($message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function error($message, array $context = array())
+    {
+        return $this->logger->err($message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function warning($message, array $context = array())
+    {
+        return $this->logger->warn($message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function notice($message, array $context = array())
+    {
+        return $this->logger->notice($message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function info($message, array $context = array())
+    {
+        return $this->logger->info($message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function debug($message, array $context = array())
+    {
+        return $this->logger->debug($message, $context);
+    }
+
+    /**
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     * @throws \InvalidArgumentException
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $denied = array('__construct', 'log');
+        if(in_array($level, $denied) OR !method_exists($this, $level)){
+            throw new \InvalidArgumentException(sprintf('Cannot log level "%s', $level));
+        }
+
+        return $this->$level($message, $context);
+    }
+}
